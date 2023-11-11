afc2ae7449b6a32b7e52b7520596c42abfa90864
Kenzy2112 <123618123+kenzy51@users.noreply.github.com>
2023-11-11 21:15:36 +0600

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://indice-dynamic-js-test.onrender.com"
+		? "https://indice-working-8quk.vercel.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
